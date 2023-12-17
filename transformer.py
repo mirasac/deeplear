4b33fcbfdc6a41ff28a538d0d028d719f5ad5185
mirasac <50930731+mirasac@users.noreply.github.com>
2023-12-17 19:23:19 +0100

    Switch to implementation as library instead of notebook

---
+++ transformer.py
@@ -0,0 +1,55 @@
+"""Implementation of Transformer model.
+
+This module contains classes and functions which implement the main parts of
+the Transformer model, as presented in the paper Attention Is All You Need
+by Vaswani et al.
+"""
+
+import math
+
+import torch
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.Module):
+    pass
+
+
+class LayerNormalization(nn.Module):
+    pass
+
+
+class FeedForwardBlock(nn.Module):
+    pass
+
+
+class MultiHeadAttention(nn.Module):
+    pass
+
+
+class ResidualConnection(nn.Module):
+    pass
+
+
+class EncoderBlock(nn.Module):
+    pass
+
+
+class Encoder(nn.Module):
+    pass
+
+
+class DecoderBlock(nn.Module):
+    pass
+
+
+class Decoder(nn.Module):
+    pass
+
+
+class ProjectionLayer(nn.Module):
+    pass
+
+
+class Transformer(nn.Module):
+    pass
